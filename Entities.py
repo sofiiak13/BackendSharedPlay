5f70993afe4a72b2baf4c82b75d9f4dac91863b8
Sofiia Khutorna <sofiiakhutorna03@gmail.com>
2025-10-13 17:55:24 -0700

    Small changes.

--- Entities.py
+++ Entities.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel
 from typing import Optional, List
 
 class User(BaseModel):
-    id: str
+    id: Optional[str] = None
     email: Optional[str] = None
     name: Optional[str] = None
     password: Optional[str] = None
@@ -10,28 +10,32 @@ class User(BaseModel):
 
 # think about what happens if someone deletes a playlist?
 class Playlist(BaseModel):
-    id: str
+    id: Optional[str] = None
     name: Optional[str] = None
-    owner: Optional[str] = None
-    editors: List[str] = []             # maybe unneccesary if have same rights as editors
     date_created: Optional[str] = None
     last_updated: Optional[str] = None
+    owner: Optional[str] = None
+    editors: List[str] = []             # maybe unneccesary if have same rights as editors
 
 class Song(BaseModel):
-    id: str
+    id: Optional[str] = None
     title: Optional[str] = None
     artist: Optional[str] = None
     added_by: Optional[str] = None
+    link: Optional[str] = None
+    playlist_id: Optional[str] = None
 
 class Comment(BaseModel):
-    id: str
+    id: Optional[str] = None
     text: Optional[str] = None
     author: Optional[str] = None
     date: Optional[str] = None
     prev: Optional[str] = None
+    song_id: Optional[str] = None
 
 class Reaction(BaseModel):
-    id: str
+    id: Optional[str] = None
     emoji: Optional[str] = None
     author: Optional[str] = None
     date: Optional[str] = None
+    comment_id: Optional[str] = None
