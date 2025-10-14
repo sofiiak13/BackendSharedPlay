7f402bc8042cee058134c6654702b2d1627e6dcf
Sofiia Khutorna <sofiiakhutorna03@gmail.com>
2025-10-13 17:53:06 -0700

    Deleted extra files.

--- test_yt.py
+++
@@ -1,6 +0,0 @@
-from main import get_yt_data  
-if __name__ == "__main__":
-    video_url = "https://www.youtube.com/watch?v=WXS-o57VJ5w&list=RDWXS-o57VJ5w"
-    # video_url = "https://music.youtube.com/watch?v=NQxS_nwPZzI&si=Nd9yOAZ30UH3UdtO"
-    result = get_yt_data(video_url)
-    print(result)
